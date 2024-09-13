4a75bd4172ec06e30e3fca5be4f3f74edeb98a77
malcosll <162439457+malcosll@users.noreply.github.com>
2024-09-13 10:05:29 -0300

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,4 @@
+idd = int(input("Digite sua idade: "))
+if idd >= 18:
+    print("Você é maior de idade.")
+else: print("Você é menor de idade.")
---
+++ ex5.py
@@ -0,0 +1,3 @@
+celsius = float(input("Digite a temperatura em Celcsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"A temperatura em fahrenheit é {fahrenheit}")
